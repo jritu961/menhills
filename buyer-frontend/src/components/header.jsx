fd4e8547b1cbad0c57a695fad6873cc049080058
jritu961 <jritu961@gmail.com>
2024-12-08 20:49:15 +0530

    fix:added header with scrolling

---
+++ buyer-frontend/src/components/header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { HeaderMainContainer, ScrollingTextWrapper, ResponsiveHeader } from "../styles/header";
+
+export const Header = () => {
+  return (
+    <HeaderMainContainer>
+      <ScrollingTextWrapper>
+        10% OFF on UPI upto ₹ 70 | Late Friday Sale live Now | Pay Via Mobikwik
+        and Get Upto 150 ₹ Cashback 
+      </ScrollingTextWrapper>
+      
+    </HeaderMainContainer>
+  );
+};
